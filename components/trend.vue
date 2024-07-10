aded354301284fdbaa9f75f45003ca3454f7a436
yucelz <yucelz@gmail.com>
2024-07-10 13:34:46 +0100

    +++

---
+++ components/trend.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="font-bold" :class="[color]">
+            {{ title }}
+        </div>
+        <div class="text-2xl font-bold font-extrabold mt-2">
+            <USkeleton class="w-full h-8" v-if="loading" />
+            <div v-else>{{ currency }}</div>
+        </div>
+        <div>
+            <USkeleton class="w-full h-6" v-if="loading" />
+            <div v-else class="flex space-x-2 items-center text-sm">
+                <UIcon :name="icon" class="w-6 h-6" :class="{ 'green': trendingUp, 'red': !trendingUp }" />
+                <div class="text-gray-500 dark:text-gray-400">
+                    {{ percentageTrend }} vs last period
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    amount: Number,
+    lastAmount: Number,
+    color: String,
+    loading: Boolean
+})
+const trendingUp = computed(
+    () => props.amount >= props.lastAmount
+)
+const icon = computed(
+    () => trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'
+)
+const percentageTrend = computed(() => {
+    if (props.amount === 0 || props.lastAmount === 0) return '0%'
+    const bigger = Math.max(props.amount, props.lastAmount)
+    const lower = Math.min(props.amount, props.lastAmount)
+    const ratio = ((bigger - lower) / lower) * 100
+    // console.log(bigger, lower, ratio, Math.ceil(ratio))
+    return `${Math.ceil(ratio)}%`
+})
+
+const { currency } = useCurrency(props.amount)
+</script>
+
+<style scoped>
+.green {
+    @apply text-green-500 dark:text-green-400;
+}
+
+.red {
+    @apply text-red-500 dark:text-red-400;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  compatibilityDate: "2024-07-09",
+  supabase: {
+    redirect: false
+  }
+});
