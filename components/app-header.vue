573a395af660c225d56eaba9827e45674e925b64
yucelz <yucelz@gmail.com>
2024-07-11 00:29:16 +0100

    ++

--- components/app-header.vue
+++ components/app-header.vue
@@ -6,7 +6,7 @@
         <div>
             <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }"
                 v-if="user">
-                <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+                <UAvatar :src="url" alt="Avatar" />
 
                 <template #account="{ item }">
                     <div class="text-left">
@@ -33,6 +33,7 @@
 <script setup>
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
+const { url } = useAvatarUrl()
 const items = [
     [{
         slot: 'account',
